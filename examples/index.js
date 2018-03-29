3d8f689428c228fa5381842add3d841510891a02
Ben <892156860@qq.com>
2018-03-29 17:53:12 +0800

    提交样式

--- examples/index.js
+++ examples/index.js
@@ -5,7 +5,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter } from 'react-router-dom';
 import App from './components/App';
-import './styles/base'
+import './styles'
 
 ReactDOM.render((
   <HashRouter>
